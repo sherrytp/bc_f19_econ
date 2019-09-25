687427b60ef0e8beeb9a3ed25d5a5c3ca108b2c6
Sherry <sherry.tp96@gmail.com>
2019-09-25 19:51:19 -0400

    changed notes

--- Forecasting/notes.R
+++ Forecasting/notes.R
@@ -7,7 +7,9 @@ cat("\f")       # Clear the console
 
 library(ggplot2)
 library(fpp2)
+library(dplyr)
 
+# 9/10
 ?goog
 # Closing stock prices of GOOG from the NASDAQ exchange, for 1000 consecutive trading days between 25 February 2013 and 13 February 2017. Adjusted for splits. goog200 contains the first 200 observations from goog.
 ?auscafe
@@ -24,3 +26,30 @@ autoplot(n)
 autoplot(sn)   # works the best 
 autoplot(rwf)
 # When and how you can calculate the predictive intervals? - covered in future lectures 
+
+
+# 9/17
+beer <- window(ausbeer, start = 1992)
+fc <- snaive(beer)
+autoplot(fc)
+
+checkresiduals(fc)
+
+# mbeer <- fc$mean
+# autoplot(mbeer)
+# NOTE: 1. mean is around 0 
+summary(residuals(fc))
+# 2. Very correlated and autocorrealted due to ACF 
+
+
+# 9/24 
+e <- tsCV(goog200, rwf, drift = TRUE, h=1)
+sqrt(mean(e^2, na.rm = TRUE))
+sqrt(mean(residuals(rwf(goog200, drift=TRUE))^2,
+          na.rm=TRUE))
+# Smaller RMSE 
+goog200 %>% 
+  tsCV(forecastingfunction = rwf, drift = TRUE, h = 1) -> e 
+e^2 %>% mean(na.rm = TRUE) %>% sqrt 
+goog200 %>% rwf(drift = TRUE) %>% residuals -> res 
+res^2 %>% mean(na.rm = TRUE, h = 1)
